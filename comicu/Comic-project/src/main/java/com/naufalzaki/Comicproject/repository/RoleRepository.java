0d72f666e3b790d82244e7a443e150697e0e3d7c
Naufalzaki05 <100655325+Naufalzaki05@users.noreply.github.com>
2022-04-25 18:41:50 +0700

    Delete comicu directory

--- comicu/Comic-project/src/main/java/com/naufalzaki/Comicproject/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.naufalzaki.Comicproject.repository;
-
-import com.naufalzaki.Comicproject.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- comicu/Comic-project/src/main/java/com/naufalzaki/Comicproject/service/framework/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.naufalzaki.Comicproject.service.framework;
-
-import com.naufalzaki.Comicproject.entity.Mail;
-
-public interface EmailService {
-    void send(Mail mail);
-}
